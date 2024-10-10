7df0524c8b068b0426178614660c1e7c4d5a425b
yuvalsherman65 <yuval.sherman@robust-sw.com>
2024-10-10 18:22:57 +0300

    Move main() to a new file.

---
+++ OpenAdvisor2.py
@@ -0,0 +1,34 @@
+import os
+from dash_chat import DashChat
+from rag import Rag
+
+
+all_config = {
+    "OUI": {
+        "title": "האוניברסיטה הפתוחה - ייעוץ כללי",
+        "description": "אני בוט הייעוץ הכללי של האוניברסיטה הפתוחה. תוכלו לשאול אותי שאלות על הלימודים באוניברסיטה הפתוחה שאינן ספציפיות לתוכנית לימודים כזו או אחרת."
+    },
+    "CS": {
+        "title": "האוניברסיטה הפתוחה - ייעוץ למדעי המחשב",
+        "description": "אני בוט הייעוץ של הפקולטה למדעי המחשב של האוניברסיטה הפתוחה. תוכלו לשאול אותי שאלות על הלימודים בפקולטה."
+    }
+}
+
+##############################################################################
+def main(faculty_code):
+    # Fetch the port from the environment
+    port = int(os.getenv('PORT', 8050))
+    config = all_config[faculty_code]
+
+    rag = Rag()
+    rag.init(faculty_code)
+
+    dash_chat = DashChat(rag)
+    title = config["title"]
+    subtitle = config["description"]
+    dash_chat.init(title, subtitle)
+
+    dash_chat.run(port)
+
+if __name__ == "__main__":
+    main("CS")
--- dash_chat.py
+++ dash_chat.py
@@ -1,21 +1,7 @@
-import os
 import dash
 import json
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
-
-from rag import Rag
-
-all_config = {
-    "OUI": {
-        "title": "האוניברסיטה הפתוחה - ייעוץ כללי",
-        "description": "אני בוט הייעוץ הכללי של האוניברסיטה הפתוחה. תוכלו לשאול אותי שאלות על הלימודים באוניברסיטה הפתוחה שאינן ספציפיות לתוכנית לימודים כזו או אחרת."
-    },
-    "CS": {
-        "title": "האוניברסיטה הפתוחה - ייעוץ למדעי המחשב",
-        "description": "אני בוט הייעוץ של הפקולטה למדעי המחשב של האוניברסיטה הפתוחה. תוכלו לשאול אותי שאלות על הלימודים בפקולטה."
-    }
-}
 
 ##############################################################################
 ##############################################################################
@@ -153,21 +139,3 @@ class DashChat:
         self._setup_callbacks()  # Move this line after setting the layout
         self.app.run_server(port=port, debug=True)
 
-##############################################################################
-def main(faculty_code):
-    # Fetch the port from the environment
-    port = int(os.getenv('PORT', 8050))
-    config = all_config[faculty_code]
-
-    rag = Rag()
-    rag.init(faculty_code)
-
-    dash_chat = DashChat(rag)
-    title = config["title"]
-    subtitle = config["description"]
-    dash_chat.init(title, subtitle)
-
-    dash_chat.run(port)
-
-if __name__ == "__main__":
-    main("CS")
