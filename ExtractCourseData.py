d78c6cf89e8a11196c0607a76f8aa1cec6c065d5
yuvalsherman65 <yuval.sherman@robust-sw.com>
2024-10-28 11:51:24 +0200

    Extract data on all courses

--- ExtractCourseData.py
+++ ExtractCourseData.py
@@ -58,6 +58,33 @@ def clean_text(text):
         return text.replace('\u200e', '').replace('\u200f', '').strip()
 
 ##############################################################################
+def get_semesters(course_id):
+    base_url = "https://www3.openu.ac.il/ouweb/owal/catalog.sel_list_semesters?kurs_in="
+    slink = base_url + course_id
+    response = requests.get(slink)
+    if response.status_code != 200:
+        return None
+    
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    try:
+        content = soup.find('ul', {'class': 'list_square'})
+        if not content:
+            return None
+        
+        semesters = []
+        for li in content.find_all('li'):
+            semesters.append(clean_text(li.get_text()))
+        return semesters
+
+    except Exception as e:
+        # Print exception
+        print(f"Error parsing course: {slink}")
+        print(e)     
+        return None
+
+
+##############################################################################
 def extract_course_data(course_url):
     response = requests.get(course_url)
     if response.status_code != 200:
@@ -82,6 +109,15 @@ def extract_course_data(course_url):
 
         course_data['course_url'] = course_url
         
+        # Is the course discontinued?
+        course_data['discontinued'] = False
+        title2 = soup.find('div', {'class': 'titles2'})
+        if (title2):
+            t2text = title2.get_text().strip()
+            if t2text == "הקורס אינו מוצע עוד":
+                course_data['discontinued'] = True
+                course_data['text'].append(t2text)
+
         content = soup.find('div', {'class': 'content'})
         if (not content):
             content = soup.find('div', {'id': 'content'})
@@ -100,15 +136,24 @@ def extract_course_data(course_url):
             # get the text of the child elewment
             child_text = child.get_text().lstrip()
 
+            # Course text includes everything in content.
+            course_data['text'].append(clean_text(child_text))
+
             # if the child tag is 'h1' and the id is 'course_title'
             if child.name == 'h1' and child.get('id') == 'course_title':
-                # Ignore. We use the class='koterettitle' instead.
+                title_text = clean_text(child_text)
+                course_id2, course_name2 = title_text.split(' ', 1)
+#                # Ignore. We use the class='koterettitle' instead.
                 continue
             elif child.name == 'p':
                 child_class = child.get('class')
                 if (child_class and 'koterettitle' in child_class):
                     title_text = clean_text(child_text)
-                    course_data['course_id'], course_data['course_name'] = title_text.split(' ', 1)
+                    if title_text == "הקורס אינו מוצע עוד":
+                        course_data['discontinued'] = True
+                        course_data['text'].append(title_text)
+                    else:
+                        course_data['course_id'], course_data['course_name'] = title_text.split(' ', 1)
                     continue
                 elif 'נקודות זכות' in child_text:
                     course_data['credits'] = clean_text(child_text)
@@ -139,6 +184,17 @@ def extract_course_data(course_url):
                         course_data['footnotes'].append(clean_text(child_text))
                     continue
 
+        if course_id2 and not course_data['course_id']:
+            course_data['course_id'] = course_id2
+        if course_name2 and not course_data['course_name']:
+            course_data['course_name'] = course_name2
+        # check if course id is a number
+        if not course_data['course_id'].isdigit():
+            # get course number from the url
+            course_data['course_name'] = course_data['course_id'] + ' ' + course_data['course_name']
+            course_data['course_id'] = course_url.split('/')[-1].split('.')[0]
+
+        # Parse classifications
         if classifications:
             for classification in classifications:
                 # split classification text by " / "
@@ -146,6 +202,14 @@ def extract_course_data(course_url):
                 for part in classification.split(' / '):
                     hierarchy.append(part)                
                 course_data['classification'].append(hierarchy)
+
+        # Find the link to the semesters page
+#        semesters = soup.find('a', {'id': 'semesters'})
+#        if semesters:
+#            slink = semesters['href']
+            sems = get_semesters(course_data['course_id'])
+            if (sems):
+                course_data['semesters'] = sems
 
         return course_data
     
@@ -166,13 +230,13 @@ def fix_dependencies(course_dependencies, course_names):
 ##############################################################################
 # Main function to extract course data for the range and save to JSON
 ##############################################################################
-def extract_all_course_data():
+def extract_all_course_data(from_id, to_id):
     print('Extracting course data...')
     base_url = "http://www.openu.ac.il/courses/{}.htm"
     courses_data = []
     course_names = {}
 
-    for course_number in range(10000, 40000):
+    for course_number in range(from_id, to_id):
         course_url = base_url.format(course_number)
         course_data = extract_course_data(course_url)
         if course_data:
@@ -192,4 +256,5 @@ def extract_all_course_data():
         json.dump(courses_data, json_file, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
-    extract_all_course_data()
+    extract_all_course_data(20991, 20993)
+#    extract_all_course_data(10000, 40000)
--- create_chroma_db.py
+++ create_chroma_db.py
@@ -102,8 +102,9 @@ def prepare_course_content(item):
     content  =  "שם הקורס: " + item["course_name"] + "\n"
     content +=  "מספר הקורס: " + item["course_id"] + "\n"
     content +=  "נקודות זכות: " + item["credits"] + "\n"
-    content +=  "שיוך ראשי: " + item["primary_classification"] + "\n"
-    content += add_list("שיוך משני: ", item["secondary_classification"])
+    classification = item["classification"]
+    for c in classification:
+        content += add_list("שיוך: ", c)
     content += add_list("קדם נדרש: ", item["required_dependencies"])
     content += add_list("קדם מומלץ: ", item["recommended_dependencies"])
     content += add_list("פרטים: ", item["text"])
@@ -123,5 +124,70 @@ def create_kb_db(faculty_list):
         print(f"Created Chroma DB for {faculty}.")
 
 ##############################################################################
+def course_stats():
+    # Get the general configuration
+    general_config = all_config["General"]
+    # Use the DB_Path from the config
+    full_path = os.path.join(general_config["DB_Path"], "all_courses.json")
+
+    # if the file does not exist - exits the function
+    if not os.path.exists(full_path):
+        print(f"File {full_path} does not exist.")
+        return
+    
+    with open(full_path, "r", encoding='utf-8') as f:
+        data = json.load(f)
+
+    # Find the min and max course numbers
+    min_course = '99999'
+    max_course = '0'
+    deps = {}
+    total_courses = len(data)
+    for item in data:
+        classifications = item['classification']
+        for c in classifications:
+            c0 = c[0]
+            if not c0:
+                print(f"Empty classification for {item['course_id']}:{item['course_name'][::-1]}")
+                continue
+
+            if c0 not in deps:
+                deps[c0] = { 'courses': [], 'faculties': {} }
+            id = item['course_id']
+            # check if the course number is a number
+            if not id.isdigit():
+                print(f"Course {id} is not a number: {item['course_name'][::-1]}")
+                continue
+            else:
+                deps[c0]['courses'].append(id)
+
+            if id < min_course:
+                min_course = id
+            if id > max_course:
+                max_course = id 
+
+            if len(c) > 1:
+                c1 = c[1]
+                if c1 not in deps[c0]['faculties']:
+                    deps[c0]['faculties'][c1] = { 'courses': [] }
+            deps[c0]['faculties'][c1]['courses'].append(id)
+
+    print(f"Total courses: {total_courses} from {min_course} to {max_course}")
+    for d in deps:
+        courses = deps[d]['courses']
+        min_courses = min(courses)
+        max_courses = max(courses)
+        print(f"{d[::-1]}: {len(courses)} courses from {min_courses} to {max_courses}")
+        #print(f"{d}: {len(courses)} courses from {min_courses} to {max_courses}")
+        for f in deps[d]['faculties']:
+            f_courses = deps[d]['faculties'][f]['courses']
+            min_f_courses = min(f_courses)
+            max_f_courses = max(f_courses)
+            print(f"    {f[::-1]}: {len(f_courses)} courses from {min_f_courses} to {max_f_courses}")
+            #print(f"    {f}: {len(f_courses)} courses from {min_f_courses} to {max_f_courses}")
+
+
+##############################################################################
 if __name__ == "__main__":
-    create_kb_db(["CS", "OUI"])
+    #create_kb_db(["CS", "OUI"])
+    course_stats()
